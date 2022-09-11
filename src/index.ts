3c3b718fcef48fe079b2e86ed460dc7f2066a691
Minung Han <hmu332233@gmail.com>
2022-09-11 16:19:02 +0900

    parseToToken 함수와 테스트 코드 추가

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,47 @@ type Token = string;
 type ConvertOptions = {
   splitSeparator?: string;
   joinSeparator?: string;
+  useMacSymbol?: boolean;
 };
+
+const MAC_KEY_MAP = {};
+
+const WINDOW_SYMBOL_MAP = {};
+const MAC_SYMBOL_MAP = {
+  meta: '⌘',
+  alt: '⌥',
+  control: '^',
+};
+
+const KEY_MAP = {
+  meta: ['command', 'cmd', '⌘'],
+  alt: ['alt', 'option', '⌥'],
+  control: ['control', 'ctrl', '^'],
+  shift: ['shift'],
+};
+
+type InvertedKeyMapType = {
+  [key: string]: string;
+};
+const INVERTED_KEY_MAP: InvertedKeyMapType = Object.entries(KEY_MAP).reduce(
+  (ret, entry) => {
+    const [key, values] = entry;
+    const valuesRet = values.reduce((valueRet, value) => {
+      return {
+        ...valueRet,
+        [value]: key,
+      };
+    }, {});
+
+    return {
+      ...ret,
+      ...valuesRet,
+    };
+  },
+  {},
+);
+
+const DEFAULT_SEPARATOR = '+';
 
 /**
  * convert
@@ -19,22 +59,46 @@ type ConvertOptions = {
  */
 export function convert(
   str: string,
-  { splitSeparator = '+', joinSeparator = '+' }: ConvertOptions = {},
+  {
+    splitSeparator = DEFAULT_SEPARATOR,
+    joinSeparator = DEFAULT_SEPARATOR,
+    useMacSymbol,
+  }: ConvertOptions = {},
 ) {
   const tokens = parseToToken(str, splitSeparator);
   return normalizeToken(tokens, joinSeparator);
 }
 
-function parseToToken(str: string, separator: string): Token[] {
-  const MOD = isIOS() ? 'Command' : 'Ctrl';
+/**
+ * shortcut string을 파싱하여, Token으로 변환한다.
+ * KeyboardEvent.key와 같은 형태이며,
+ * $mod는 OS에 타입에 따라 meta/control로 변환된다.
+ *
+ * @param str A Shortcut string
+ * @param separator A string that identifies character or characters to use in separating the string
+ * @returns Token[]
+ */
+export function parseToToken(
+  str: string,
+  separator: string = DEFAULT_SEPARATOR,
+): Token[] {
+  const mod = isIOS() ? 'meta' : 'control';
   return str
     .trim()
     .split(separator)
+    .map((v) => v.toLowerCase())
     .map((key) => {
-      return key === '$mod' ? MOD : key;
+      if (key === '$mod') {
+        return mod;
+      }
+      return INVERTED_KEY_MAP[key] || key;
     });
 }
 
-function normalizeToken(tokens: Token[], separator: string) {
+function normalizeToken(
+  tokens: Token[],
+  separator: string = DEFAULT_SEPARATOR,
+  useSymbol?: boolean,
+) {
   return tokens.join(separator);
 }
--- test.js
+++ test.js
@@ -1,20 +1,77 @@
 'use strict';
-const { convert } = require('./dist/index.js');
+const { convert, parseToToken } = require('./dist/index.js');
 const { isIOS } = require('./dist/utils.js');
 
 jest.mock('./dist/utils.js');
 
-describe('convert', () => {
-  test('Platform이 Mac일 경우, Mac shortcut 표기법에 맞게 표시된다.', () => {
+describe('parseToToken', () => {
+  test('platform이 Mac일 경우, $mod가 meta로 변환된다.', () => {
     isIOS.mockImplementation(() => true);
-    expect(convert('$mod')).toBe('Command');
-    expect(convert('Shift+K')).toBe('Shift+K');
-    expect(convert('$mod+Shift+K')).toBe('Command+Shift+K');
+    expect(parseToToken('$mod')).toEqual(['meta']);
   });
-  test('Platform이 Window일 경우, window shortcut 표기법에 맞게 표시된다.', () => {
+  test('platform이 Window일 경우, $mod가 control로 변환된다.', () => {
     isIOS.mockImplementation(() => false);
-    expect(convert('$mod')).toBe('Ctrl');
-    expect(convert('Shift+K')).toBe('Shift+K');
-    expect(convert('$mod+Shift+K')).toBe('Ctrl+Shift+K');
+    expect(parseToToken('$mod')).toEqual(['control']);
+  });
+  test('Meta를 나타내는 키워드들이 Meta로 변환된다.', () => {
+    expect(parseToToken('command+cmd+⌘')).toEqual(['meta', 'meta', 'meta']);
+    expect(parseToToken('Command+Cmd+⌘')).toEqual(['meta', 'meta', 'meta']);
+    expect(parseToToken('COMMAND+CMD+⌘')).toEqual(['meta', 'meta', 'meta']);
+  });
+  test('Alt를 나타내는 키워드들이 Alt로 변환된다.', () => {
+    expect(parseToToken('alt+option+⌥')).toEqual(['alt', 'alt', 'alt']);
+    expect(parseToToken('Alt+Option+⌥')).toEqual(['alt', 'alt', 'alt']);
+    expect(parseToToken('ALT+OPTION+⌥')).toEqual(['alt', 'alt', 'alt']);
+  });
+  test('Control을 나타내는 키워드들이 Control로 변환된다.', () => {
+    expect(parseToToken('control+ctrl+^')).toEqual([
+      'control',
+      'control',
+      'control',
+    ]);
+    expect(parseToToken('Control+Ctrl+^')).toEqual([
+      'control',
+      'control',
+      'control',
+    ]);
+    expect(parseToToken('CONTROL+CTRL+^')).toEqual([
+      'control',
+      'control',
+      'control',
+    ]);
   });
 });
+
+// describe('convert - platform이 Mac일 경우', () => {
+//   test('Mac shortcut 표기법에 맞게 표시된다.', () => {
+//     isIOS.mockImplementation(() => true);
+//     expect(convert('$mod')).toBe('Command');
+//     expect(convert('Shift+K')).toBe('Shift+K');
+//     expect(convert('$mod+Shift+K')).toBe('Command+Shift+K');
+//   });
+//   test('splitSeparator가 존재할 경우, 해당 문자열 기준으로 잘라낸다.', () => {
+//     isIOS.mockImplementation(() => true);
+//     const options = {
+//       splitSeparator: '-',
+//     };
+//     expect(convert('Shift-K', options)).toBe('Shift+K');
+//     expect(convert('$mod-Shift-K', options)).toBe('Command+Shift+K');
+//   });
+//   test('joinSeparator가 존재할 경우, 해당 문자열을 사용해서 병합된다.', () => {
+//     isIOS.mockImplementation(() => true);
+//     const options = {
+//       joinSeparator: '-',
+//     };
+//     expect(convert('Shift+K', options)).toBe('Shift-K');
+//     expect(convert('$mod+Shift+K', options)).toBe('Command-Shift-K');
+//   });
+// });
+
+// describe('convert - platform이 Window/Server일 경우', () => {
+//   test('window shortcut 표기법에 맞게 표시된다.', () => {
+//     isIOS.mockImplementation(() => false);
+//     expect(convert('$mod')).toBe('Ctrl');
+//     expect(convert('Shift+K')).toBe('Shift+K');
+//     expect(convert('$mod+Shift+K')).toBe('Ctrl+Shift+K');
+//   });
+// });
