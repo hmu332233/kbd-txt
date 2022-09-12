5528ba39a66f35f8e7ad935fae70b4a8e9524258
Minung Han <hmu332233@gmail.com>
2022-09-13 02:36:29 +0900

    맥을 감지하지 못하던 버그 수정

--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,7 @@
 export function isIOS() {
   return (
     typeof navigator === 'object' &&
-    /Mac|iPod|iPhone|iPad/.test(
+    /mac|Mac|iPod|iPhone|iPad/.test(
       navigator?.userAgentData?.platform || navigator?.platform || '-',
     )
   );
